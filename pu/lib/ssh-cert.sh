dccebc1558a8be6ce9a39c68a54cf4dff81959a1
shivaraj-bh <me@shivaraj-bh.in>
2026-04-30 10:02:32 +0000

    Add MACs manually because kandji is stupid

--- pu/lib/ssh-cert.sh
+++ pu/lib/ssh-cert.sh
@@ -1,8 +1,10 @@
 client_auth_init() {
   _pu_instance_ssh_opts=()
+  _pu_mac_opts=(-o "MACs=hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com")
 
   if [ "${PU_USE_SSH_CA:-}" != "true" ]; then
-    _pu_ssh_opts=(-o StrictHostKeyChecking=no)
+    _pu_ssh_opts=("${_pu_mac_opts[@]}" -o StrictHostKeyChecking=no)
+    _pu_instance_ssh_opts=("${_pu_mac_opts[@]}")
     return
   fi
 
@@ -15,8 +17,8 @@ client_auth_init() {
     step ssh certificate --force --no-agent --no-password --insecure me "$PU_STATE_DIR/key"
   fi
 
-  _pu_instance_ssh_opts=(-i "$PU_STATE_DIR/key" -o "CertificateFile=$PU_STATE_DIR/key-cert.pub" -o IdentitiesOnly=yes)
-  _pu_ssh_opts=(-i "$PU_STATE_DIR/key" -o "CertificateFile=$PU_STATE_DIR/key-cert.pub" -o IdentitiesOnly=yes \
+  _pu_instance_ssh_opts=("${_pu_mac_opts[@]}" -i "$PU_STATE_DIR/key" -o "CertificateFile=$PU_STATE_DIR/key-cert.pub" -o IdentitiesOnly=yes)
+  _pu_ssh_opts=("${_pu_mac_opts[@]}" -i "$PU_STATE_DIR/key" -o "CertificateFile=$PU_STATE_DIR/key-cert.pub" -o IdentitiesOnly=yes \
     -o "UserKnownHostsFile=$PU_STATE_DIR/known_hosts" -o StrictHostKeyChecking=accept-new)
 }
 
